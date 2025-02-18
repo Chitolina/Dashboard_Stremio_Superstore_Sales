9df278649d5f5ae1a3d428d685c72506c2f358db
Lucas Chitolina <lucaschito.dados@gmail.com>
2025-02-18 17:07:45 -0300

    Update Superstore_Analise_Dashboard.py

--- Superstore_Analise_Dashboard.py
+++ Superstore_Analise_Dashboard.py
@@ -347,7 +347,7 @@ st.markdown("---")
 
 st.write("**Fonte dos Dados:** [Kaggle - Superstore Dataset](https://www.kaggle.com/datasets/vivek468/superstore-dataset-final)")
 
-st.write("**Desenvolvido por:** [Lucas Chitolina¹](https://github.com/Chitolina), [Lucas Chitolina²](https://chitolina.github.io/), & [ChatGpt](https://chatgpt.com/)")
+st.write("**Desenvolvido por:** [Lucas Chitolina¹](https://github.com/Chitolina), [Lucas Chitolina²](https://chitolina.github.io/), & [ChatGPT](https://chatgpt.com/)")
 
 # Terceira aba ===========================================================================
 with tab3:
