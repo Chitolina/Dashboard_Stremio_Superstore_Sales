d973d89fbc579f9405f99d4db0acbb94977f0870
Lucas Chitolina <lucaschito.dados@gmail.com>
2025-02-19 16:24:44 -0300

    Update Superstore_Analise_Dashboard.py

--- Superstore_Analise_Dashboard.py
+++ Superstore_Analise_Dashboard.py
@@ -299,7 +299,7 @@ with tab2:
     
     fig1, ax1 = plt.subplots(figsize=(14, 8))
     sns.scatterplot(data=df, x='Vendas', y='Lucro', hue='Categoria', 
-                    palette='husl', alpha=0.7, s=200, edgecolor='black', linewidth=1.5)
+                    palette='mako', alpha=0.7, s=200, edgecolor='black', linewidth=1.5)
     sns.regplot(data=df, x='Vendas', y='Lucro', scatter=False, 
                 color='red', line_kws={'linewidth':2, 'linestyle':'--'})
     
@@ -327,7 +327,7 @@ with tab2:
     
     fig2, ax2 = plt.subplots(figsize=(10, 5))
     barplot = sns.barplot(data=df, x='Categoria', y='Margem de Lucro', 
-                          palette='husl', alpha=0.7, estimator=np.mean)
+                          palette='mako', alpha=0.7, estimator=np.mean)
     
     # Adiciona os valores médios dentro de cada barra
     for p in barplot.patches:
